6f17874239f34cbc9ba45cdb63265cb2793db1de
AleksandraArt <an78art@gmail.com>
2020-12-11 18:01:43 +0300

    v.1

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean add = true;
+        int balance = 100;
+        int refill = 1100;
+        int bonus = refill/ 100;
+
+            if (add) {
+                int total = refill + balance + bonus;
+                System.out.println(total);
+
+            }else{
+                 int total = refill + balance;
+                 System.out.println(total);
+
+            }
+
+
+
+
+    }
+
+}
+
