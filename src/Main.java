312a4296934a14c3b4343b95e425248a12811c1e
AleksandraArt <an78art@gmail.com>
2020-12-11 20:34:50 +0300

    v.1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        boolean add = true;
         int balance = 100;
-        int refill = 1100;
+        int refill = 1000;
         int bonus = refill/ 100;
 
-            if (add) {
+            if (refill >= 1000) {
                 int total = refill + balance + bonus;
                 System.out.println(total);
 
